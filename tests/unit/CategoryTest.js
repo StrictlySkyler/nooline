7e8310af6f75bf870668dca586cf3bde7ec335a6
Skyler Brungardt <skyler.brungardt@gmail.com>
2014-06-01 11:03:01 -0700

    Updated unit tests.

--- tests/unit/CategoryTest.js
+++ tests/unit/CategoryTest.js
@@ -1,19 +1,34 @@
 var assert = require("assert");
 
 describe('A Category', function () {
-  var Category = require('../common/js/nooline/models/category.js');
-  var CategoryView = require('../common/js/nooline/views/category-view.js');
+  var Category = require('../../common/js/nooline/models/category.js');
+  var CategoryView = require('../../common/js/nooline/views/category-view.js');
+  var Snippets = require('../../common/js/nooline/collections/snippets.js');
   var _category;
 
   it('should be able to be created', function () {
 
     _category = new Category();
 
-    assert(_category instanceof Category);
+    assert(
+      _category instanceof Category, 
+      '_category is not an instance of Category model.'
+    );
   });
 
   it('should have a view attached to it', function () {
 
-    assert(_category.view instanceof CategoryView);
+    assert(
+      _category.view instanceof CategoryView,
+      'Looks like the view is missing, or the wrong thing.'
+    );
+  });
+
+  it('should contain a collection of content', function () {
+
+    assert(
+      _category.get('snippets') instanceof Snippets, 
+      'No collection of content found, or it is the wrong thing.'
+    );
   });
 });
